3c2f9b3d1cfb1e833e254043b3584f0aed64ac94
daiki-murakmai <muranokami1@gmail.com>
2019-09-29 16:58:13 +0900

    route,dbの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/index'
+  get 'users/new'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get 'pages/index'
+  get 'pages/new'
+  get '/create', to: 'pages#new'
+  post '/create',  to: 'pages#create'
+  root 'static_pages#home'
+  get 'static_pages/home'
+  get 'static_pages/profile'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  resources :users
+  resources :pages
 end
